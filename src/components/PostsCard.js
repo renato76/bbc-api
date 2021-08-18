58c961196ce746502a76d981ac69674e4976dd5b
renato76 <renmignogna@gmail.com>
2021-08-18 23:11:41 +0100

    install moment and add 24 hour format

--- src/components/PostsCard.js
+++ src/components/PostsCard.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import moment from 'moment'
 
-const PostsCard = ({ owner, tags }) => {
-  console.log(tags)
+const PostsCard = ({ owner, tags, publishDate }) => {
+  console.log(tags, publishDate)
   return (
     <div className="card-container">
       <div className="card">
@@ -9,18 +10,22 @@ const PostsCard = ({ owner, tags }) => {
           <div className="avatar">
             <img className="avatar" src={owner.picture} />
           </div>
-          <div className="name">
-            <div className="title">
-              <h2>{owner.title}</h2>
+          <div className="name-date-container">
+            <div className="name">
+              <div className="title">
+                <h2>{owner.title}</h2>
+              </div>
+              <div className="firstname">
+                <h2>{owner.firstName}</h2>
+              </div>
+              <div className="lastname">
+                <h2>{owner.lastName}</h2>
+              </div>
             </div>
-            <div className="firstname">
-              <h2>{owner.firstName}</h2>
-            </div>
-            <div className="lastname">
-              <h2>{owner.lastName}</h2>
+            <div className="publish-date">
+              <h2>{moment(publishDate).format('Do MMM YY HH:mm:ss')}</h2>
             </div>
           </div>
-        
         </div>
         
       </div>
