4d5f9b9452a236e1ba6c27cf29aaa420d5041262
renato76 <renmignogna@gmail.com>
2021-08-19 16:34:22 +0100

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { getAllData } from './lib/api'
+import { getAllData, getByTag } from './lib/api'
 import PostsCard from './components/PostsCard'
 
 const App = () => {
-
   const [posts, setPosts] = useState([])
+  const [search, setSearch] = useState('')
+  const [searchHistory, setSearchHistory] = useState(localStorage.getItem('search-history').split(',') || []) 
+
+  useEffect(() => {
+    localStorage.setItem('search-history', searchHistory)
+  }, [searchHistory])
 
   useEffect(() => {
     async function getData() {
-      const res = await getAllData()
+      const res = await getAllData('')
       const data = await res.data
       const results = data.data
       setPosts(results)
@@ -16,12 +21,44 @@ const App = () => {
     getData()
   }, [])
 
+  async function getSearchData() {
+    // 1. send search with what they are typing
+    const res = await getByTag(search)
+    // 2. set that terms in state
+    setSearchHistory([...searchHistory, search])
+    // 3. state needs to sync with local storage
+    const data = await res.data
+    console.log(data)
+    const results = data.data
+    setPosts(results)
+  }
+
   console.log(posts)
 
   return (
     <div className="main-container">
       <div className="main-title">
         <h1>BBC Paws</h1>
+      </div>
+      <div>
+        <input 
+          value={search}
+          onChange={(e) => { 
+            setSearch(e.target.value) 
+          }}
+        />
+        <button
+          onClick={getSearchData}
+        >Search
+        </button>
+        <div>
+          {searchHistory.map(term => {
+            return <button key={term}>{term}</button>
+          })}
+        </div>
+        <div>
+          <button onClick={() => setSearchHistory([])}>Clear Search</button>
+        </div>
       </div>
       <div className="posts">
         {posts.map((post, index) => 
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 
-export const getAllData = () => {
-  return axios.get('https://dummyapi.io/data/v1/post?limit=30', {
+export const getAllData = (search) => {
+  return axios.get('https://dummyapi.io/data/v1/post', {
     headers: {
       'app-id': '6116d5e6400e5154e7aec763',
     },    
+    params: {
+      limit: 30,
+      text: search,
+    },
+  })
+}
+
+export const getByTag = (tag) => {
+  return axios.get(`https://dummyapi.io/data/v1/tag/${tag}/post`, {
+    headers: {
+      'app-id': '6116d5e6400e5154e7aec763',
+    },    
+    params: {
+      limit: 30,
+    },
   })
 }
