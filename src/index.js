66e4631c8fabbe13674d417ea12c3518813f4869
renato76 <renmignogna@gmail.com>
2021-08-22 23:26:12 +0100

    add internet explorer IE11 in the package json browserlist

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/main.scss'
 import App from './App'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 
 ReactDOM.render(<App />, document.getElementById('root'))
